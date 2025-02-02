52e9ef592a82e8347572bce374797ed650516186
csbarbado <50236211+csbarbado@users.noreply.github.com>
2025-02-02 17:16:35 -0300

    Aula 4

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) { /* recebe o que foi digitado no input */
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name; /*pega o elemento name da api */
+        artistImage.src = element.urlImg; /*pega o elemento urlimg  da api*/
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');/*esconde a playlist */
+        resultArtist.classList.remove('hidden');/*mostra o artista*/
+        return
+    }
+    
+    requestApi(searchTerm);
+})
